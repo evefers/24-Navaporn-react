b943f4fae43329b4bcf552449724ce59ec53d070
evefers <evefers@gmail.com>
2025-12-12 21:22:19 +0700

    upload owner pic

--- src/views/Owner.jsx
+++ src/views/Owner.jsx
@@ -9,7 +9,7 @@ const Owner = () => {
 
           <h1 className="text-2xl font-bold text-gray-800 mb-2">24 Navaporn (Eve) #JSD11</h1>
           <div className="w-32 h-32 bg-blue-500 rounded-full flex items-center justify-center text-white text-5xl font-bold mx-auto mb-6">
-            <img src={"../public/cat.jpg"} alt="cat" width="500" height="600" />
+            <img src={"cat.jpg"} alt="cat" width="500" height="600" />
           </div>
           <div className="mt-8 pt-6 border-t border-gray-200 max-w-2xl mx-auto">
             <h2 className="text-xl text-gray-800 mb-4">Short Biography:</h2>
